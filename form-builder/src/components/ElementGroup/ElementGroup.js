a6e911f163208c31cc4d47c08726a4cc2d277cd6
Pranjal Choudhary <pranjalchoudhary@Pranjal-Choudharys-MacBook-Air.local>
2021-12-26 15:37:21 +0530

    feature : search functionality

--- form-builder/src/components/ElementGroup/ElementGroup.js
+++ form-builder/src/components/ElementGroup/ElementGroup.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import { templates } from "../../constants/consts";
+import React, { useState, useEffect } from "react";
+import { Form, InputGroup, Spinner } from "react-bootstrap";
+import { Search } from "react-bootstrap-icons";
+import { elements } from "../../constants/consts";
 import { ElementConfiguration } from "../ElementConfiguration";
 import { NavElementsList } from "../NavElementsList";
 import { NavTemplatesList } from "../NavTemplatesList";
 
 export const ElementGroup = () => {
   const [modal, setModal] = useState(null);
+  const [renderedElements, setRenderedElements] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchText.trim() === "") {
+      setRenderedElements(elements);
+    }
+  }, [searchText]);
+
+  useEffect(() => {
+    setRenderedElements(elements);
+  }, []);
 
   // updates state of modal
   function toggleModal(val) {
@@ -16,16 +30,60 @@ export const ElementGroup = () => {
       setModal(val);
     }
   }
-  return (
-    <div className="p-3" style={{ overflow: "scroll", height: "85vh" }}>
-      {modal && (
-        <ElementConfiguration modal={modal} toggleModal={toggleModal} />
-      )}
-      <p className="text-white">Basic Elements</p>
 
-      <NavElementsList toggleModal={toggleModal} />
-      <p className="text-white">Templates</p>
-      <NavTemplatesList toggleModal={toggleModal} />
-    </div>
+  let timer = null;
+  function debounceWrapper(event) {
+    setLoading(true);
+    if (timer !== null) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      searchElement(event);
+    }, 500);
+  }
+
+  // search elements
+  function searchElement(event) {
+    let val = event.target.value;
+    setSearchText(val);
+    let filteredElem = elements.filter((item) =>
+      item.buttonTitle.toLowerCase().includes(val)
+    );
+    setLoading(false);
+    setRenderedElements(filteredElem);
+  }
+
+  return (
+    <>
+      <InputGroup>
+        <InputGroup.Text className="bg-dark border-0">
+          <Search size={10} color={"white"} />
+        </InputGroup.Text>
+        <Form.Control
+          className="bg-dark border-0 text-white"
+          size="sm"
+          type="text"
+          placeholder="Search"
+          onChange={(e) => debounceWrapper(e)}
+        />
+        {loading && (
+          <InputGroup.Text className="bg-dark border-0">
+            <Spinner animation="border" variant="white" size="sm" />
+          </InputGroup.Text>
+        )}
+      </InputGroup>
+      <div className="p-3" style={{ overflow: "scroll", height: "85vh" }}>
+        {modal && (
+          <ElementConfiguration modal={modal} toggleModal={toggleModal} />
+        )}
+        <p className="text-white">Basic Elements</p>
+        <NavElementsList
+          toggleModal={toggleModal}
+          elements={renderedElements}
+        />
+        {/* <p className="text-white">Templates</p>
+        <NavTemplatesList toggleModal={toggleModal} /> */}
+      </div>
+    </>
   );
 };
--- form-builder/src/components/NavElementsList/NavElementsList.js
+++ form-builder/src/components/NavElementsList/NavElementsList.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { elements } from "../../constants/consts";
 
-export const NavElementsList = ({ toggleModal }) => {
-  return elements.map((item, index) => {
-    return (
-      <Button
-        key={index}
-        variant="outline-info"
-        className="w-100 mb-3"
-        onClick={() => toggleModal(item.type)}
-      >
-        {item.buttonTitle}
-      </Button>
-    );
-  });
+export const NavElementsList = ({ toggleModal, elements }) => {
+  return elements.length > 0 ? (
+    elements.map((item, index) => {
+      return (
+        <Button
+          key={index}
+          variant="outline-info"
+          className="w-100 mb-3"
+          onClick={() => toggleModal(item.type)}
+        >
+          {item.buttonTitle}
+        </Button>
+      );
+    })
+  ) : (
+    <div className="text-muted d-flex justify-content-center mt-5">
+      <p>
+        <small>Not matching element found</small>
+      </p>
+    </div>
+  );
 };
